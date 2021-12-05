911d8832357434cf81859119b945067f34456773
valqua2007 <quanduongforworks2007@gmail.com>
2021-12-05 14:39:00 +0700

    điều hướng giữa các pages sử dụng Link trong Nextjs

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import Link from "next/link";
 const Navbar = () => {
     return (
         <nav>
             <div>
-                <a href="#">Coder Wikipedia</a>
+                <Link href="/">Coder Wikipedia</Link>
             </div>
             <div>
-                <a href="#">About</a>
-                <a href="#">All Coders</a>
+                <Link href="/about">About</Link>
+                <Link href="/coders">All Coders</Link>
             </div>
         </nav>
     );

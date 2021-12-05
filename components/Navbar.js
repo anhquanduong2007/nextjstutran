04903096e21a9978716164a3b31564362c705352
valqua2007 <quanduongforworks2007@gmail.com>
2021-12-05 14:32:33 +0700

    tạo navbar và footer component

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#">Coder Wikipedia</a>
+            </div>
+            <div>
+                <a href="#">About</a>
+                <a href="#">All Coders</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Navbar />
       <h1>Home page</h1>
       <p>lorem</p>
-    </div>
+      <Footer />
+    </>
   )
 }
